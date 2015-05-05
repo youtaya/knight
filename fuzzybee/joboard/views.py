de07c725ff2b4ef33cecca1666e86fc3035f4b54
youtaya <jxiaoping@gmail.com>
2015-05-05 20:10:04 +0800

    form build

--- fuzzybee/joboard/views.py
+++ fuzzybee/joboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render_to_response, render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 from joboard.models import Factory
 from joboard.forms import FactoryForm
+from django.template import RequestContext
 
 import logging
 logger = logging.getLogger(__name__)
@@ -14,10 +16,10 @@ def index(request):
             factory = form.cleaned_data
             logger.debug("name: " + factory['fact_name'])
             logger.debug("address: " + factory['fact_addr'])
-            return HttpResponseRedirect('/board/detail/')
+            return HttpResponseRedirect(reverse('board:detail'))
     else:
         form = FactoryForm()
-    return render_to_response('board/new.html', {'form': form})
+    return render_to_response('board/new.html', {'form': form}, context_instance=RequestContext(request))
 
 def detail(request):
     return render(request, 'board/detail.html')
